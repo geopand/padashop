34b215ebc5483f5fc08c021dc79cb6fe2a340daa
George Pandelias <pandeliasg@gmail.com>
2025-01-30 00:25:12 +0200

    Initial commit

---
+++ src/main/java/gr/padashop/PadashopApplication.java
@@ -0,0 +1,13 @@
+package gr.padashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadashopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/padashop/PadashopApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.padashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
