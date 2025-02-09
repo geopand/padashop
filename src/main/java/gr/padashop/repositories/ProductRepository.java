b9185492fd0fb80c2b633c30a849becb541f0c94
George Pandelias <pandeliasg@gmail.com>
2025-02-09 12:14:00 +0200

    implement produce creation in db

--- src/main/java/gr/padashop/repositories/ProductRepository.java
+++ src/main/java/gr/padashop/repositories/ProductRepository.java
@@ -2,6 +2,7 @@ package gr.padashop.repositories;
 
 
 import gr.padashop.models.Product;
+import gr.padashop.models.ProductCategory;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,26 +22,40 @@ public class ProductRepository implements CrudRepository<Product> {
 
     @Override
     public List<Product> getAll() {
-        return List.of();
+        String sql = "SELECT * FROM products";
+        return jdbcClient.sql(sql).query(Product.class).list();
     }
+
+    //todo get all by product
+
+    //todo getBySlug
 
     @Override
     public void create(Product product) {
 
-    }
+        String sql = "INSERT into products (name, description, slug, category, picture, price) VALUES(?,?,?,?,?,?)";
+        jdbcClient.sql(sql).params(List.of(
+                product.getName(),
+                product.getDescription(),
+                product.getSlug(),
+                product.getCategory(),
+                product.getPicture() == null ? "" : product.getPicture(),
+                product.getPrice()))
+                .update();
+}
 
-    @Override
-    public void update(Product product) {
+@Override
+public void update(Product product) {
 
-    }
+}
 
-    @Override
-    public void delete(String id) {
+@Override
+public void delete(String id) {
 
-    }
+}
 
-    @Override
-    public Optional<Product> getById(String id) {
-        return Optional.empty();
-    }
+@Override
+public Optional<Product> getById(String id) {
+    return Optional.empty();
+}
 }
