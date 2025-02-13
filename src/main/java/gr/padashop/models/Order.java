75575acc541c47d643730a79708592e780e3ccdd
George Pandelias <pandeliasg@gmail.com>
2025-02-13 19:59:29 +0200

    feat: Introduce models: order and order-item

--- src/main/java/gr/padashop/models/Order.java
+++ src/main/java/gr/padashop/models/Order.java
@@ -6,11 +6,168 @@ public class Order {
 
     private Long id;
     private String userId;
-    private String ccName;
+    private List<OrderItem> items;
     private String ccType;
+    private String ccName;
     private String ccNumber;
-    private String ccExpiration;
-    private List<OrderDetail> details;
+    private int ccExpiryMoth;
+    private int ccExpiryYear;
+    private int ccCVC;
+    private boolean isSelfPickUp;
+    private String street;
+    private String streetNumber;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public String getCcType() {
+        return ccType;
+    }
+
+    public void setCcType(String ccType) {
+        this.ccType = ccType;
+    }
+
+    public String getCcName() {
+        return ccName;
+    }
+
+    public void setCcName(String ccName) {
+        this.ccName = ccName;
+    }
+
+    public String getCcNumber() {
+        return ccNumber;
+    }
+
+    public void setCcNumber(String ccNumber) {
+        this.ccNumber = ccNumber;
+    }
+
+    public int getCcExpiryMoth() {
+        return ccExpiryMoth;
+    }
+
+    public void setCcExpiryMoth(int ccExpiryMoth) {
+        this.ccExpiryMoth = ccExpiryMoth;
+    }
+
+    public int getCcExpiryYear() {
+        return ccExpiryYear;
+    }
+
+    public void setCcExpiryYear(int ccExpiryYear) {
+        this.ccExpiryYear = ccExpiryYear;
+    }
+
+    public int getCcCVC() {
+        return ccCVC;
+    }
+
+    public void setCcCVC(int ccCVC) {
+        this.ccCVC = ccCVC;
+    }
+
+    public boolean isSelfPickUp() {
+        return isSelfPickUp;
+    }
+
+    public void setSelfPickUp(boolean selfPickUp) {
+        isSelfPickUp = selfPickUp;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", userId='" + userId + '\'' +
+                ", items=" + items +
+                ", ccType='" + ccType + '\'' +
+                ", ccName='" + ccName + '\'' +
+                ", ccNumber='" + ccNumber + '\'' +
+                ", ccExpiryMoth=" + ccExpiryMoth +
+                ", ccExpiryYear=" + ccExpiryYear +
+                ", ccCVC=" + ccCVC +
+                ", isSelfPickUp=" + isSelfPickUp +
+                ", street='" + street + '\'' +
+                ", streetNumber='" + streetNumber + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/gr/padashop/models/OrderItem.java
@@ -0,0 +1,82 @@
+package gr.padashop.models;
+
+import java.math.BigDecimal;
+
+public class OrderItem {
+    private Long id;
+    private Long orderId;
+    private Product product;
+    private Integer quantity;
+    private BigDecimal price;
+    private String itemStatus;
+    private Long shippingDate; //epoch
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getItemStatus() {
+        return itemStatus;
+    }
+
+    public void setItemStatus(String itemStatus) {
+        this.itemStatus = itemStatus;
+    }
+
+    public Long getShippingDate() {
+        return shippingDate;
+    }
+
+    public void setShippingDate(Long shippingDate) {
+        this.shippingDate = shippingDate;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "id=" + id +
+                ", orderId=" + orderId +
+                ", product=" + product +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", itemStatus='" + itemStatus + '\'' +
+                ", shippingDate=" + shippingDate +
+                '}';
+    }
+}
